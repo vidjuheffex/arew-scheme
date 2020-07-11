12c9bf3f3ca6c92318d2f8b3683a4d282f9470b4
amirouche <amirouche@hyper.dev>
2020-07-11 20:33:11 +0200

    arew/data/finger-tree: wanna be fast

---
+++ src/arew/data/finger-tree.scm
@@ -0,0 +1,38 @@
+(define-library (arew data finger-tree)
+  (import
+   (scheme generator)
+   (scheme base)
+   (scheme case-lambda)
+   (scheme cxr)
+   (scheme lazy)
+   (scheme list))
+
+  (export
+   make-finger-tree
+   finger-tree
+   finger-tree?
+   finger-tree-empty?
+   finger-tree-force
+   finger-tree-append
+   finger-tree-filter
+   finger-tree-fold-left
+   finger-tree-fold-right
+   finger-tree-for-each
+   finger-tree-length
+   finger-tree-map
+   finger-tree-reverse
+   finger-tree-left
+   finger-tree-right
+   finger-tree-add-left
+   finger-tree-add-right
+   finger-tree-remove-left
+   finger-tree-remove-right
+   finger-tree-scan
+   finger-tree-split
+   generator->finger-tree
+   list->finger-tree
+   finger-tree->generator
+   finger-tree->reverse-generator
+   finger-tree->list)
+
+  (include "arew/data/finger-tree/body.scm"))
