ee636c11d0bd6aa865ebe3780fa02f81131607be
amirouche <amirouche@hyper.dev>
2020-07-14 17:42:10 +0200

    scheme foo: add missing re-export of r7rs libraries as (scheme foo).

---
+++ src/scheme/sort.scm
@@ -0,0 +1,11 @@
+(library (scheme sort)
+
+  (export
+    list-sorted? vector-sorted? list-merge vector-merge list-sort vector-sort
+    list-stable-sort vector-stable-sort list-merge! vector-merge! list-sort!
+    vector-sort! list-stable-sort! vector-stable-sort!
+    list-delete-neighbor-dups vector-delete-neighbor-dups
+    list-delete-neighbor-dups! vector-delete-neighbor-dups! vector-find-median
+    vector-find-median!)
+
+  (import (srfi srfi-132)))
