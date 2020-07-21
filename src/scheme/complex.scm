de2d0ff7f2b453ab86e987b5c931490e73be1e8b
amirouche <amirouche@hyper.dev>
2020-07-21 23:49:36 +0200

    scheme complex: replace rnrs with chezscheme.

--- src/scheme/complex.scm
+++ src/scheme/complex.scm
@@ -1,9 +1,7 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 ;; SPDX-License-Identifier: CC0-1.0
-#!r6rs
-
 (library (scheme complex)
   (export
     angle imag-part magnitude make-polar make-rectangular real-part)
   (import
-    (rnrs)))
+    (chezscheme)))
