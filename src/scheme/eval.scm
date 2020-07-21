6ad161d390bacfbae321cc3ee6fa9f98a8786b2a
amirouche <amirouche@hyper.dev>
2020-07-21 23:50:33 +0200

    scheme eval: replace (rnrs eval) with (chezscheme).

--- src/scheme/eval.scm
+++ src/scheme/eval.scm
@@ -1,7 +1,5 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 ;; SPDX-License-Identifier: CC0-1.0
-#!r6rs
-
 (library (scheme eval)
   (export environment eval)
-  (import (rnrs eval)))
+  (import (chezscheme)))
