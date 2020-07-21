46b876676f763a0f50e667a312ef51b94d4f0579
amirouche <amirouche@hyper.dev>
2020-07-21 23:51:54 +0200

    scheme inexact: replace rnrs with chezscheme.

--- src/scheme/inexact.scm
+++ src/scheme/inexact.scm
@@ -1,28 +1,26 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 ;; SPDX-License-Identifier: CC0-1.0
-#!r6rs
-
 (library (scheme inexact)
   (export
    acos asin atan cos exp finite? infinite? log nan? sin sqrt tan)
   (import
-   (except (rnrs) finite? infinite? nan?)
-   (prefix (rnrs) r6:))
+   (except (chezscheme) finite? infinite? nan?)
+   (prefix (chezscheme) cs:))
 
   (define (finite? z)
     (if (complex? z)
-        (and (r6:finite? (real-part z))
-             (r6:finite? (imag-part z)))
-        (r6:finite? z)))
+        (and (cs:finite? (real-part z))
+             (cs:finite? (imag-part z)))
+        (cs:finite? z)))
 
   (define (infinite? z)
     (if (complex? z)
-        (or (r6:infinite? (real-part z))
-            (r6:infinite? (imag-part z)))
-        (r6:infinite? z)))
+        (or (cs:infinite? (real-part z))
+            (cs:infinite? (imag-part z)))
+        (cs:infinite? z)))
 
   (define (nan? z)
     (if (complex? z)
-        (or (r6:nan? (real-part z))
-            (r6:nan? (imag-part z)))
-        (r6:nan? z))))
+        (or (cs:nan? (real-part z))
+            (cs:nan? (imag-part z)))
+        (cs:nan? z))))
