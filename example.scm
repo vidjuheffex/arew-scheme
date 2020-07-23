601c5d8e832975d1262f2e5e20248ff312d1e977
amirouche <amirouche@hyper.dev>
2020-07-23 14:05:51 +0200

    miniml example that reproduce the problem

--- example.scm
+++ example.scm
@@ -1,6 +1,6 @@
-(import (scheme base))
+(import (chezscheme))
+(import (m))
 
-(let loop ((index 100))
-  (unless (zero? index)
-    (pk "hello world")
-    (loop (- index 1))))
+
+(let-syntax ((define (vanish-define define (cons*))))
+  #t)
