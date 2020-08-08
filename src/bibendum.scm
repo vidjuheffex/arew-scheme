5db29a127f5fb1efce1d8e0d4a9885af926c0507
amirouche <amirouche@hyper.dev>
2020-08-08 16:26:26 +0200

    bibendum: make it useable.

--- src/bibendum.scm
+++ src/bibendum.scm
@@ -9,8 +9,11 @@
 (import (scheme time))
 (import (scheme process-context))
 
-(import (only (chezscheme) random random-seed))
+(import (arew matchable))
 
+(import (only (chezscheme)
+              random random-seed file-exists?
+              file-regular? fasl-read fasl-write))
 
 (define (read-lines filename)
   (call-with-input-file filename
@@ -20,14 +23,6 @@
         (if (eof-object? line)
             (reverse out)
             (loop (read-line port) (cons line out)))))))
-
-(define (maybe-human-message line)
-  (let loop ((line (drop (string->list line) 21)))
-    (cond
-     ((null? line) #f)
-     ((char=? (car line) #\space) #f)
-     ((char=? (car line) #\:) (list->string (cddr line)))
-     (else (loop (cdr line))))))
 
 (define (string-downcase string)
   (list->string (map char-downcase (string->list string))))
@@ -65,8 +60,7 @@
 (define (make-hash-table*)
   (make-hash-table (make-default-comparator)))
 
-
-(define (kb-store kb grams)
+(define (kb-store! kb grams)
   (let loop ((grams grams))
     (unless (null? grams)
       (let* ((gram (car grams))
@@ -74,25 +68,7 @@
              (value (hash-table-ref/default kb key 0)))
         (hash-table-set! kb key (+ value 1))
         (loop (cdr grams))))))
-                       
-(define filename "data/scheme-2020-04.txt")
 
-(define kb (make-hash-table*))
-
-(let loop ((lines (read-lines filename)))
-  (unless (null? lines)
-    (let ((maybe (maybe-human-message (car lines))))
-      (when maybe
-        (let* ((tokens (string-tokenize (string-replace-punctuation (string-downcase maybe))))
-               (tokens* (append (list "%") tokens (list "%")))
-               (grams (append-map (lambda (n) (ngrams tokens* n)) (cddr (iota (min 5 (length tokens*)))))))
-          (kb-store kb grams)))
-      (loop (cdr lines)))))
-
-(define (big pair)
-  (< 1 (cdr pair)))
-
-(define kb* (hash-table->alist kb))
 
 (define (choice list)
   (list-ref list (random (length list))))
@@ -116,17 +92,65 @@
         (apply string-append (reverse (cdr out)))
         (loop (cdr strings) (cons " " (cons (car strings) out))))))
 
-(random-seed (modulo (exact (current-second)) (expt 2 32)))
+(define (learn input output)
+  (define kb (make-hash-table*))
 
-(let loop ((sentence (list "%" "r7rs" "is"))
-           (prefix 1))
-  (if (zero? prefix)
-      (pk (string-join sentence))
-      (let ((next (continue* kb* (drop sentence (- (length sentence) prefix)))))
-        (cond
-         ((not next)
-          (loop sentence
-                (- prefix 1)))
-         ((string=? next "%") (pk (string-join sentence)))
-         (else (loop (append sentence (list next))
-                     (+ (length sentence) 1)))))))
+  (unless (and (file-exists? input) (file-regular? input))
+    (error "input is not legit!" input))
+
+  (when (and (file-exists? output) (file-regular? output))
+    (let ((port (open-binary-input-file output)))
+      (set! kb (alist->hash-table (fasl-read port) (make-default-comparator)))
+      (close-port port)))
+
+  (let loop ((lines (read-lines input)))
+    (unless (null? lines)
+      (let* ((line (car lines))
+             (tokens (string-tokenize (string-replace-punctuation (string-downcase line))))
+             (tokens* (append (list "%") tokens (list "%")))
+             (grams (append-map (lambda (n) (ngrams tokens* n)) (cddr (iota (min 5 (length tokens*)))))))
+        (kb-store! kb grams)
+        (loop (cdr lines)))))
+
+  (when (file-exists? output)
+    (delete-file output))
+
+  (let ((port (open-binary-output-file output)))
+    (fasl-write (hash-table->alist kb) port)
+    (close-port port))
+
+  (display "done!\n"))              
+  
+(define (generate filename . start)
+  (define oops (random-seed (modulo (exact (current-second)) (expt 2 32))))
+
+  (define port (open-binary-input-file filename))
+
+  (define kb (fasl-read port))
+
+  (let loop ((sentence (car start))
+             (prefix 1))
+    (if (zero? prefix)
+        (begin (display (string-join sentence)) (newline))
+        (let ((next (continue* kb (drop sentence (- (length sentence) prefix)))))
+          (cond
+           ((not next)
+            (loop sentence
+                  (- prefix 1)))
+           ((string=? next "%") (loop sentence 0))
+           (else (loop (append sentence (list next))
+                       (+ (length sentence) 1)))))))
+
+  (close-port port))
+
+(define (help)
+  (display "Usage:\n")
+  (display "\n")
+  (display "  learn INPUT OUTPUT\n")
+  (display "  generate INPUT [SOME FUN ...]\n"))
+
+(match (cdr (command-line))
+  (("learn" input output) (learn input output))
+  (("generate" input . start) (generate input start))
+  (else (help)))
+   
